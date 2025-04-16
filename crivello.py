6eef6a17e151510bfa7135d2abd8af78f09e5988
OWGROUP\rghitti <roberto.ghitti@objectway.com>
2025-04-16 21:39:24 +0200

    first commit

---
+++ crivello.py
@@ -0,0 +1,13 @@
+# Algoritmo per trovare i numeri primi fino a n
+# Funzione che restituisce una lista di numeri primi fino a n
+def crivello(n):
+    l = list(range(2, n + 1))
+    for i in range(2, int(n**0.5) + 1):
+        if i in l:
+            for j in range(i * 2, n + 1, i):
+                if j in l:
+                    l.remove(j)
+    return l
+
+print(crivello(100))
+
